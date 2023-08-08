3b16e20a0aa967a62e01824e49e074e1feabd2c3
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-08-09 01:28:00 +0300

    Add files via upload

---
+++ routers/stands.router.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const axios = require('axios');
+
+const router  = express.Router();
+
+
+let leagues = {
+     premierLeague: 2021,
+     ucl: 2001,
+     ligue1: 2015,
+     bundesliga: 2002,
+     serieA: 2019,
+     laliga: 2014
+}
+
+async function getStanding(leagueCode) {
+     try {
+          const config = {
+               method: 'get',
+               maxBodyLength: Infinity,
+               url: `https://api.football-data.org/v3/competitions/${leagueCode}/standings`,
+               headers: { 
+                 'Content-Type': 'application/json', 
+                 'X-Auth-Token': 'a89d53abb2da4790bdc6cd3a5f27529a'
+               }
+             };
+           
+            await axios(config)
+             .then(async function (response)  {
+           
+               responseData = await response.data;
+           
+               seasonInfo = responseData.season
+           
+               standings = responseData.standings[0].table
+           
+               return responseData, standings, seasonInfo
+           })
+     } catch (error) {
+          console.log(error)
+     }
+}
+
+async function getMatches(leagueCode) {
+     try {
+          const config = {
+               method: 'get',
+               maxBodyLength: Infinity,
+               url: `https://api.football-data.org/v4/competitions/${leagueCode}/matches?matchday=${seasonInfo.currentMatchday}`,
+               headers: { 
+                 'Content-Type': 'application/json', 
+                 'X-Auth-Token': 'a89d53abb2da4790bdc6cd3a5f27529a'
+               }
+             };     
+           
+            await axios(config)
+             .then(async function (response)  {
+           
+              responseMatches = await response.data;
+           
+              matches = responseMatches.matches
+               return responseMatches, matches
+           })
+     } catch (error) {
+          console.log(error)
+     }
+  
+   
+}
+   
+router.get('/laliga-table', async function(req, res) {
+
+     try {
+         let data = await getStanding(leagues.laliga);
+         let mtchs = await getMatches(leagues.laliga);
+          res.render('./standings/laliga.ejs', {
+               standings, seasonInfo, matches,
+               error: null,
+               pageTitle: "ترتيب الدوري الاسباني لاليغا"
+            });
+     } catch (error) {
+          res.render('./standings/laliga.ejs', {
+               standings:null, seasonInfo:null, matches:null,
+               error:"Failed to load, server connection lost",
+               pageTitle: "حدث خطأ في جلب الدوري الاسباني"
+            });
+     }
+});
+   
+router.get('/bundesliga-table', async function(req, res) {
+
+     try {
+         let stands = await getStanding(leagues.bundesliga);
+         let mtchs = await getMatches(leagues.bundesliga);
+        
+          res.render('./standings/bundesliga.ejs', {
+             standings, seasonInfo, matches,
+             error: null,
+             pageTitle: "ترتيب الدوري الالماني البوندسليغا"
+          });
+     } catch (error) {
+          res.render('./standings/bundesliga.ejs', {
+               standings:null, seasonInfo:null, matches:null,
+               error: "Failed to load the standing, server connection lost",
+               pageTitle: "حدث خطأ في جلب الدوري الالماني"
+            }); 
+     }
+});
+   
+router.get('/seriea-table', async function(req, res) {
+
+     try {
+         let stands = await getStanding(leagues.serieA);
+         let mtchs = await getMatches(leagues.serieA);
+          res.render('./standings/serieA.ejs', {
+             standings, seasonInfo, matches,
+             error: null,
+             pageTitle: "ترتيب الدوري الايطالي"
+          });
+     } catch (error) {
+          res.render('./standings/serieA.ejs', {
+               standings:null, seasonInfo:null, matches:null,
+               error: "Failed to load the standing, server connection lost",
+               pageTitle: "حدث خطأ في جلب الدوري الايطالي"
+            }); 
+     }
+});
+   
+router.get('/premierleague-table', async function(req, res) {
+
+     try {
+         let stands = await getStanding(leagues.premierLeague);
+         let mtchs = await getMatches(leagues.premierLeague);
+          res.render('./standings/premierLeague.ejs', {
+             standings, seasonInfo, matches,
+             error: null,
+             pageTitle: "ترتيب الدوري الانكليزي الممتاز البريميرليغ"
+          });
+     } catch (error) {
+          res.render('./standings/premierLeague.ejs', {
+               standings:null, seasonInfo:null, matches:null,
+               error: "Failed to load the standing, server connection lost",
+               pageTitle: "حدث خطأ في جلب الدوري الانكليزي"
+            }); 
+     }
+});
+   
+router.get('/liguea-table', async function(req, res) {
+
+     try {
+         let stands = await getStanding(leagues.ligue1);
+         let mtchs = await getMatches(leagues.ligue1);
+          res.render('./standings/liguea.ejs', {
+             standings, seasonInfo, matches,
+             error: null,
+             pageTitle: "ترتيب الدوري الفرنسي ليغ 1"
+          });
+     } catch (error) {
+          res.render('./standings/liguea.ejs', {
+               standings:null, seasonInfo:null, matches:null,
+               error: "Failed to load the standing, server connection lost",
+               pageTitle: "حدث خطأ في جلب الدوري الفرنسي"
+            }); 
+     }
+});
+
+module.exports = router;
---
+++ routers/ucl.router.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const axios = require('axios');
+const moment = require("moment");
+
+const router  = express.Router();
+
+async function getMatches() {
+     try {
+          
+          let url = `https://api.football-data.org/v4/competitions/2001/matches`;
+          
+          await axios.get(url, { headers: { "X-Auth-Token": "a89d53abb2da4790bdc6cd3a5f27529a" } })
+          .then(async function(response) {
+               fixtures = response.data.matches
+               return fixtures
+          });
+
+     } catch (error) {
+          console.log(error)
+     }
+}
+
+async function getGroupMatches() {
+     try {
+          
+          let url = `https://api.football-data.org/v4/competitions/2001/standings`;
+          
+          await axios.get(url, { headers: { "X-Auth-Token": "a89d53abb2da4790bdc6cd3a5f27529a" } })
+          .then(async function(response) {
+               groups = response.data
+               return groups
+          });
+
+     } catch (error) {
+          console.log(error)
+     }
+}
+
+async function getStats() {
+     try {
+          
+          let url = `https://webws.365scores.com/web/stats/?appTypeId=5&langId=27&timezoneName=Asia/Baghdad&userCountryId=114&competitions=572&competitors=&withSeasons=true`;
+          
+          await axios.get(url)
+          .then(async function(response) {
+               stats = response.data
+               return stats
+          });
+
+     } catch (error) {
+          console.log(error)
+     }
+}
+
+async function getBrackets() {
+     try {
+          
+          let url = `https://api.football-data.org/v4/competitions/2001/matches?stage=LAST_16,QUARTER_FINALS,SEMI_FINALS,FINAL`;
+          
+          await axios.get(url, { headers: { "X-Auth-Token": "a89d53abb2da4790bdc6cd3a5f27529a" } })
+          .then(async function(response) {
+               brackets = response.data
+               return brackets
+          });
+
+     } catch (error) {
+          console.log(error)
+     }
+}
+
+router.get("/ucl", async (req, res) => {
+
+     try {
+          await getStats()
+          await getMatches()
+          
+          res.render("./ucl/ucl.ejs", {
+               error: null,
+               pageTitle: "دوري ابطال اوروبا - مباريات ونتائج واحصائيات"
+          });
+          
+                    
+     } catch (error) {
+          res.render("./ucl/ucl.ejs", {
+               error: "Failed to load, server connection lost",
+               pageTitle: "حدث خطأ في جلب المعلومات"
+          });
+          console.log(error);
+     }
+});
+
+router.get("/ucl/stats", async (req, res) => {
+
+     try {
+
+          await getStats()
+          
+          res.render("./ucl/stats.ejs", {
+               error: null,
+               pageTitle: "الهدافون واحصائيات اخرى دوري ابطال اوروبا"
+          });
+          
+                    
+     } catch (error) {
+          res.render("./ucl/stats.ejs", {
+               error: "Failed to load, server connection lost",
+               pageTitle: "حدث خطأ في جلب المعلومات"
+          });
+          console.log(error);
+     }
+});
+
+router.get("/ucl/brackets", async (req, res) => {
+
+     try {
+          await getBrackets()
+
+          res.render("./ucl/brackets.ejs", {
+               error: null, moment,
+               pageTitle: "دوري ابطال اوروبا - مرحلة الاقصائيات", brackets
+          });
+          
+     } catch (error) {
+          res.render("./ucl/brackets.ejs", {
+               error: "حدث خطأ اثناء جلب المعلومات", moment,
+               pageTitle: "حدث خطأ في جلب المعلومات"
+          });
+          console.log(error);
+     }
+});
+
+router.get("/ucl/groups", async (req, res) => {
+
+     try {
+
+          await getGroupMatches();
+          res.render("./ucl/groups.ejs", {
+               error: null, moment,
+               pageTitle: "دوري ابطال اوروبا - دور المجموعات"
+          });
+          
+                    
+     } catch (error) {
+          res.render("./ucl/groups.ejs", {
+               error: "Failed to load, server connection lost", moment,
+               pageTitle: "حدث خطأ في جلب المعلومات"
+          });
+          console.log(error);
+     }
+});
+
+module.exports = router; 
