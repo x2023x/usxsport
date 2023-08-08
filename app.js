d7c9ed2ad27bf5cc3069730ff489f94bf46a4bbd
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-08-09 01:20:40 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var proxy = require('html2canvas-proxy');
+const app = express();
+var moment = require('moment');
+moment().format();
+app.locals.moment = moment;
+
+const cors = require("cors");
+const path = require("path");
+const { Telegraf, Telegram } = require("telegraf");
+
+const standsRoute = require('./routers/stands.router.js');
+const matchsRoute = require('./routers/matchday.router.js');
+const uclRoute = require('./routers/ucl.router.js');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(cors());
+app.use(standsRoute)
+app.use(matchsRoute)
+app.use(uclRoute)
+app.use('/ucl', proxy());
+app.use('/matchday', proxy());  
+
+app.get('/', function(req, res) {
+  res.render('index.ejs', {
+    
+  });
+});
+
+app.listen(5000);
