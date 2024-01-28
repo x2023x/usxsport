8955457c91fd7ab334149fd69db55b20a24f74d2
x2023x <149948978+x2023x@users.noreply.github.com>
2024-01-28 02:23:21 +0000

    e

--- models/Channels.model.js
+++ models/Channels.model.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
-const channelSchema = new mongoose.Schema({ name: String, status: Boolean, logo: String, link: String  });
-
-
+const channelSchema = new mongoose.Schema({ 
+    name: String, 
+    match: String,
+    status: Boolean, 
+    provider: String, 
+    link: String, 
+    status: String  
+});
 
 const Channel = new mongoose.model('Channel', channelSchema);
 module.exports = Channel;
