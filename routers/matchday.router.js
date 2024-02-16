2118fa875424183238002114cf5368b41a7ca5bf
x2023x <149948978+x2023x@users.noreply.github.com>
2024-02-16 00:43:39 +0000

    s

--- routers/matchday.router.js
+++ routers/matchday.router.js
@@ -18,6 +18,7 @@ let leagues = {
 }
 
 async function getFixtures(date) {
+
      try {
           let url = `https://api.football-data.org/v4/matches?competitions=${leagues.ucl},${leagues.laliga},${leagues.bundesliga},${leagues.serieA},${leagues.ligue1},${leagues.premierLeague},${leagues.eredivisie},${leagues.primeiraLiga},${leagues.championship}&date=${date}`;
 
@@ -32,7 +33,6 @@ async function getFixtures(date) {
           await axios.get(url, { headers: { "X-Auth-Token": "a89d53abb2da4790bdc6cd3a5f27529a" } })
           .then(async function(response) {
                fixtures = response.data.matches
-               console.log(fixtures)
                return fixtures
           });
 
@@ -41,23 +41,57 @@ async function getFixtures(date) {
      }
 }
 
+async function getEventsByDay(date){
+     try {
+
+          let url = `https://api.sofascore.com/api/v1/sport/football/scheduled-events/${date}`
+         
+          if(date == undefined || date == null){
+               url = `https://api.sofascore.com/api/v1/sport/football/scheduled-events/${(moment().format("YYYY-MM-DD"))}`
+          }
+          if(moment(date).isValid() == false){
+               url = `https://api.sofascore.com/api/v1/sport/football/scheduled-events/${(moment().format("YYYY-MM-DD"))}`
+          }
+
+          await axios.get(url, {
+               headers: {
+                    "Accept": "*/*",
+                    "Accept-Encoding": "br; q=1.0, gzip;q=0.9, deflate;q=0.8",
+                    "Cache-Control": "max-age=0",
+                    "Accept-Language": "ar-IQ",
+                    "Connection": "keep-alive",
+                    "Host": "api.sofascore.com",
+                    "If-None-Match": 'W/"28477692fb"',
+                    "User-Agent": "SofascoreApp/6.7.29 (com.SofaScore.iOS; build:20240202.54; iOS 17.3.0) Alamofire/5.8.0 84c"
+               }
+          })
+          .then(async function(response){
+               events = response.data
+               return events
+          })
+          return events
+     } catch (error) {
+          console.log(error)
+     }
+}
 
 router.get("/matchday", async (req, res) => {
 
      try {
-
           let date = req.query.date;
-          
-          await getFixtures(date)
+          let events = await getEventsByDay(date);
+     
           res.render("./matchs/matchday.ejs", {
                error: null,
-               pageTitle: "مباريات اليوم و مباريات الغد"
+               pageTitle: "مباريات اليوم و مباريات الغد",
+               events
           });
 
      } catch (error) {
           res.render("./matchs/matchday.ejs", {
                error: "Failed to load matches, server connection lost",
-               pageTitle: "حدث خطأ في جلب المباريات"
+               pageTitle: "حدث خطأ في جلب المباريات",
+               events: null,
           });
           console.log(error);
      }
